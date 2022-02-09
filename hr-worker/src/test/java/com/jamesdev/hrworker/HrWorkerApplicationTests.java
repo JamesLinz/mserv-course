10890640392b111d68a17cfc7a7661abaf1c92c4
James Lins <jlalencar@gmail.com>
2022-02-09 14:45:43 -0300

    Projeto hr-worker criado

---
+++ hr-worker/src/test/java/com/jamesdev/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesdev.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
